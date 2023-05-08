76ba3154009af454930d97374c0c72d55b0a05ed
adhithyan <3e3ee@gmail.com>
2023-05-08 17:10:31 +0530

    dssd

---
+++ eclipse-workspace/AssistedPractice1/src/AssistedPractice2/Main.java
@@ -0,0 +1,11 @@
+package AssistedPractice2;
+
+public class Main {
+	 public static void main(String[] args) {
+	        MyClass obj = new MyClass();
+	        System.out.println(obj.publicVar);
+	        obj.publicMethod();
+
+	
+	    }
+	}
---
+++ eclipse-workspace/AssistedPractice1/src/AssistedPractice2/MyClass.java
@@ -0,0 +1,26 @@
+package AssistedPractice2;
+
+public class MyClass {
+    public int publicVar = 10;
+    private int privateVar = 20;
+    protected int protectedVar = 30;
+    int defaultVar = 40;
+
+    public void publicMethod() {
+        System.out.println("This is a public method.");
+    }
+
+    private void privateMethod() {
+        System.out.println("This is a private method.");
+    }
+
+    protected void protectedMethod() {
+        System.out.println("This is a protected method.");
+    }
+
+    void defaultMethod() {
+        System.out.println("This is a default method.");
+    }
+}
+
+
