5d12aafde66a236ef01e51cd9c356ce692636671
adhithyan <3e3ee@gmail.com>
2023-05-09 02:28:37 +0530

    dads

---
+++ eclipse-workspace/AssistedPractice1/src/AssistedPractice1/Typecast.java
@@ -0,0 +1,19 @@
+package AssistedPractice1;
+
+public class Typecast {
+	public static void main(String[] args) {
+        //Implicit type casting
+        int x = 11;
+        double y = x;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        //Explicit type casting
+        double a = 3.18;
+        int b = (int) a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
+
+
